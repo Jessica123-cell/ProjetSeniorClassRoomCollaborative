e97287e4c63773907f1556acff746865235a7684
ponguenewende <jessblessed226@gmail.com>
2025-11-26 22:35:07 +0100

    Seul le professeur peut changer les pages de la diapo principale

--- Assets/MainScreenController.cs
+++ Assets/MainScreenController.cs
@@ -1,7 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainScreenController : MonoBehaviour
 {
+    [Header("Diapo Buttons")]
+    public Button nextPageBTN;
+    public Button prevPageBTN;
+
+    void Start()
+    {
+        // Récupérer le joueur local
+        var player = PlayerRoleManager.LocalPlayer;
+
+        if (player == null)
+        {
+            Debug.LogWarning("[MainScreenController] LocalPlayer is NULL at Start!");
+            return;
+        }
+
+        // Vérifier si le joueur est enseignant
+        bool isTeacher = player.CurrentRole == Role.Teacher;
+
+        // Seul le professeur peut cliquer les boutons
+        nextPageBTN.interactable = isTeacher;
+        prevPageBTN.interactable = isTeacher;
+    }
+
     public void NextPage()
     {
         SmartUnivManager.instance.OnNextPage();
@@ -12,3 +36,4 @@ public class MainScreenController : MonoBehaviour
         SmartUnivManager.instance.OnPrevPage();
     }
 }
+
